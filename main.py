89b8f379747311aee95802bc7d97cc7b7a4afbd3
Sinmu <3038267725@qq.com>
2024-04-18 16:21:14 +0000

    添加发送图片函数

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import os
+import os, json, time
 import random
 
 from starlette.responses import RedirectResponse
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from requests_toolbelt import MultipartEncoder
 import uvicorn
 
 from CusImage import CusImage
 from Translate import YouDao
 from dotenv import load_dotenv
+from requests import request
 
 
 load_dotenv()
@@ -57,10 +59,79 @@ async def random_word():
     return {"word": word}
 
 
+def get_feishu_tenant_access_token():
+    url = "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal"
+    data = {
+        "app_id": os.getenv("FEISHU_APP_ID"),
+        "app_secret": os.getenv("FEISHU_APP_SECRET")
+    }
+    payload = json.dumps(data)
+
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+
+    response = request("POST", url, headers=headers, data=payload)
+    if response.status_code != 200:
+        return {"error": False, "msg":"获取 tenant_access_token 失败", "data": response.text}
+    res = response.json()
+    return res.get("tenant_access_token", None)
+
+def upload_img_to_feishu(tenant_access_token):
+    print("tenant_access_token: ", tenant_access_token)
+    if tenant_access_token == None:
+        return {"error":True, "msg":"无法获取feishu的tenant_access_token"}
+    
+    url = "https://open.feishu.cn/open-apis/im/v1/images"
+    form = {'image_type': 'message',
+            'image': (open('static/english.png', 'rb'))}  # 需要替换具体的path 
+    multi_form = MultipartEncoder(form)
+    headers = {
+        'Authorization': f"Bearer {tenant_access_token}",  ## 获取tenant_access_token, 需要替换为实际的token
+    }
+    headers['Content-Type'] = multi_form.content_type
+    response = request("POST", url, headers=headers, data=multi_form)
+
+
+    if response.status_code != 200:
+        return {"error": True,"msg":"上传图片失败","data": response.text}
+    res = response.json()
+
+    img_key = res.get("data",{}).get("image_key",None)
+    return img_key
+
+
+
+def send_msg_feishu():
+    tenant_access_token = get_feishu_tenant_access_token()
+    img_key = upload_img_to_feishu(tenant_access_token=tenant_access_token)
+    if not img_key:
+        return {"error": True, "msg":"无法获取上传图片的 key"}
+    url = "https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=open_id"
+    data = {
+        "receive_id": os.getenv("FEISHU_RECEIVE_ID"),
+        "msg_type": "image",
+        "content": json.dumps({
+            "image_key":f"{img_key}"
+        }),
+        "uuid": str(int(time.time()))
+    }
+    payload = json.dumps(data)
+
+    headers = {
+        'Authorization': f"Bearer {tenant_access_token}",
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+
+    response = request("POST", url, headers=headers, data=payload)
+
+    return {"error": False, "data": response.json()}
+
+
+
 @app.get("/")
 async def index():
     return {"status":"running..."}
 
 if __name__ == '__main__':
-
     uvicorn.run("main:app", port=5000, host="0.0.0.0")
